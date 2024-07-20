0fcac7bd9c53f115c6f7bc29dc1eb83bf344f869
David Garcia <david.garcia@deralia.com>
2024-07-20 06:23:34 +0200

    Umami

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@
 
 import { usePathname } from 'next/navigation'
 import { redirect } from 'next/navigation'
-import { i18n } from "@/i18n-config";
+import { i18n } from "@/i18n-config"
+import Script from "next/script"
 
 export default function HomeLayout({children}: {children: React.ReactNode}) {
     const pathname = usePathname()
@@ -12,6 +13,9 @@ export default function HomeLayout({children}: {children: React.ReactNode}) {
     return (
         <html>
             <body>{children}</body>
+            <Script defer
+                    src="https://cloud.umami.is/script.js"
+                    data-website-id="01cc0ffe-5550-492e-8796-33b4327e56dc" />
         </html>
     );
 }
